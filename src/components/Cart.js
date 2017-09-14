54fa45ca29a983c7fc310cbab54fd0b9faab452c
stephan.marais <stephan.marais@entelect.co.za>
2017-09-14 07:47:44 +0200

    Add Cart element and new tests to make sure the cart works properly

---
+++ src/components/Cart.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Cart = (props) => {
+  var ItemCount = props.items.length;
+  var TotalCost = props.items.map(s=>s.price).reduce((a,b)=>a+b,0);
+  return (
+    <div className="Cart">
+      <ul>
+        {props.items.map((i,x)=>(<li onClick={()=>(props.onRemoveItem(x))}>{i.brand}>{i.name}</li>))}
+      </ul>
+    </div>
+  )
+};
+
+Cart.propTypes = {
+  items: React.PropTypes.array.isRequired
+};
+
+export default Cart;
