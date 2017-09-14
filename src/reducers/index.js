457870c4a6a6e50a9f22d1d6e17484e204388165
stephan.marais <stephan.marais@entelect.co.za>
2017-09-14 11:54:08 +0200

    add displayShoeReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import {combineReducers} from 'redux';
 import cartReducer from './cartReducer';
 import shoeReducer from './shoeReducer';
+import displayShoesReducer from './displayShoeReducer';
 const rootreducer = combineReducers({    
     cart:cartReducer,
     shoes:shoeReducer,
+    displayShoes:displayShoesReducer,
 });
 
 export default rootreducer;
