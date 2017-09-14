5898a055a75561dd1034253c6d86cdbff9f82f82
stephan.marais <stephan.marais@entelect.co.za>
2017-09-14 14:05:46 +0200

    remove display shoes from redux store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,10 @@
 import {combineReducers} from 'redux';
 import cartReducer from './cartReducer';
 import shoeReducer from './shoeReducer';
-import displayShoesReducer from './displayShoeReducer';
 import facetSelectedReducer from './facetSelectedReducer';
 const rootreducer = combineReducers({    
     cart:cartReducer,
     shoes:shoeReducer,
-    displayShoes:displayShoesReducer,
     facetSelected:facetSelectedReducer,
 });
 
