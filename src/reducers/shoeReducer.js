891eedab464f1ee503ac8aa86b54554b3b0481ce
Stephan Marais <stephan.marais@entelect.co.za>
2017-09-14 20:16:13 +0200

    clean main branch

--- src/reducers/shoeReducer.js
+++
@@ -1,9 +0,0 @@
-const shoeReducer = function(state=[],action){
-    if(action.type==="SHOES_SET")
-    {
-        return action.payload.slice();
-    }
-    return state;
-}
-
-export default shoeReducer;
