38c132cce368cc69b27bc3523b5c89a540aaaa7a
stephan.marais <stephan.marais@entelect.co.za>
2017-09-14 12:02:33 +0200

    add facetSelectedReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,12 @@ import {combineReducers} from 'redux';
 import cartReducer from './cartReducer';
 import shoeReducer from './shoeReducer';
 import displayShoesReducer from './displayShoeReducer';
+import facetSelectedReducer from './facetSelectedReducer';
 const rootreducer = combineReducers({    
     cart:cartReducer,
     shoes:shoeReducer,
     displayShoes:displayShoesReducer,
+    facetSelected:facetSelectedReducer,
 });
 
 export default rootreducer;
